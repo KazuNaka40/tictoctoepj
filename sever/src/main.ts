876e17dc3251ab12e48d1158aafb289eb507e55c
sukeko <venusgeneration@gmail.com>
2020-09-28 09:43:50 +0900

    userID add

--- sever/src/main.ts
+++ sever/src/main.ts
@@ -4,20 +4,19 @@ import socketio from "socket.io";
 //ここでまってるよ、リッスン//
 const io = socketio(3000);
 
-const players: string[] = [];
+const players = new Set<string>();
+
 let turn = 0;
 
 io.on("connection", (socket: socketio.Socket) => {
-  if (players.length >= 2) {
+  /*if (players.length >= 2) {
     socket.disconnect(true);
     console.log("disconnect");
     return;
   }
-  players.push(socket.id);
-  console.log(players);
-  io.emit("sendMessageToClient", socket.id);
-  socket.on("clicked", (position: number) => {
-    if (socket.id == players[turn % 2]) {
+  players.push(1socket.id);*/
+  socket.on("clicked", (position: number, userID: string) => {
+    if (userID == Array.from(players)[turn % 2]) {
       io.emit("clickedAll", position, socket.id);
       console.log(position, socket.id);
       turn++;
@@ -25,6 +24,15 @@ io.on("connection", (socket: socketio.Socket) => {
   });
   socket.on("initGame", () => {
     reset(io);
+  });
+  socket.on("userID", (userID: string) => {
+    console.log(userID);
+    if (!players.has(userID) && players.size >= 2) {
+      socket.disconnect(true);
+      console.log("disconnect");
+      return;
+    }
+    players.add(userID);
   });
 });
 
